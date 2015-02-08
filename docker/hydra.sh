d66d5cac471c7cb504c8d5627d9554bd567ffec4
Marcos Cano <mcano@stsa.info>
2015-02-07 23:01:59 -0600

    Sat Feb  7 23:01:59 CST 2015

--- docker/hydra.sh
+++ docker/hydra.sh
@@ -5,9 +5,9 @@ log_dir="/var/log/dockemu"
 log_file=${1:-"${RANDOM}.log"}
 log_file=${log_dir}/${log_file}
 
+echo "LOGFILE:" $log_file
+
 echo "[STARTING  $(date +%D-%T) ]" | tee -a $log_file
-
-
 
 
 hydra -L /passwords -V -P /passwords -t 64 $server ssh | tee -a $log_file
