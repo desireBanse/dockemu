aafe8140f7a8dfa51b660fa2833e864af8185a3d
Marcos Cano <mcano@stsa.info>
2015-02-07 23:06:03 -0600

    Sat Feb  7 23:06:03 CST 2015

--- docker/hydra.sh
+++ docker/hydra.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 log_dir="/var/log/dockemu"
-
 log_file=${1:-"${RANDOM}"}
+
+name=$log_file
 log_file=${log_dir}/${log_file}.log
 
 echo "LOGFILE:" $log_file
 
-echo "[STARTING BOT: $log_file $(date +%D-%T) ]" | tee -a $log_file
+echo "[STARTING BOT: $name $(date +%D-%T) ]" | tee -a $log_file
 
 sleep 1
 
