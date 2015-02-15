3f718da396f66d2f53a3d3f3ffe3cc589ce19ebf
Marcos Cano <mcano@stsa.info>
2015-02-15 00:56:49 -0600

    Sun Feb 15 00:56:49 CST 2015

--- badguys/files/forever.sh
+++ badguys/files/forever.sh
@@ -7,18 +7,21 @@ echo "running $duration"
 
 
 
-server=${1:-"0.0.0.0"}
+server=${1:-$attack_servers}
 duration=${2:-60}
 
 count=0
 while  ! [ true -a  $count -gt $duration ];do 
-	python attackftp.py -t $server -p users
-	sleep 1
-	echo " "
-	echo "Attacking server"
-	echo "$count NTH TIME"
-	echo "################################"
-        echo " "
+	
+	for ip in $server;do
+		python attackftp.py -t $server -p users
+		sleep 1
+		echo " "
+		echo "Attacking server $ip"
+		echo "$count NTH TIME"
+		echo "################################"
+	    echo " "
+	done
 	count=$(($count + 1))
 done
 
