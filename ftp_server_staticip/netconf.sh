22f9466f5b5f27e0b7862a8703100fb854ab6573
Marcos Cano <mcano@stsa.info>
2015-02-15 00:10:12 -0600

    Sun Feb 15 00:10:12 CST 2015

--- ftp_server_staticip/netconf.sh
+++ ftp_server_staticip/netconf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ip addr flush dev eth0
+
 echo "auto eth0" >> /etc/network/interfaces
 
 echo "iface eth0 inet static" >> /etc/network/interfaces
@@ -10,7 +12,6 @@ echo "netmask 255.255.0.0" >> /etc/network/interfaces
 
 echo "gateway 172.17.42.1" >> /etc/network/interfaces
  
-
 ifup eth0
 
 /etc/init.d/networking restart
